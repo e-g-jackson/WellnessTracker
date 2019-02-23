cae2feeed9c22b7439eefc61c76eb0d1aa24c1ff
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-23 03:47:04 -0600

    npm-run-build

---
+++ client/build/precache-manifest.6bf5fb98fc40b66e23d7111256f06307.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a29e0c52359ba3d24b1c",
+    "url": "/static/js/main.a29e0c52.chunk.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "a29e0c52359ba3d24b1c",
+    "url": "/static/css/main.00566d1d.chunk.css"
+  },
+  {
+    "revision": "890e0c4e1f7f3e6570e9d3337a61b180",
+    "url": "/index.html"
+  }
+];
