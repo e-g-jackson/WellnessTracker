92148f3c1b9d1443fa5b62f0a94ae57122b4efc3
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-25 01:08:53 -0600

    Beginning Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from "./assets/Header";
 import Graph from "./assets/Graph";
 import Body from "./assets/Body";
 import './App.css';
-import Login from './assets/Login';
-import Profile from './assets/Profile';
-import Food from './assets/Food';
-import Types from './assets/Types';
-import Progress from './assets/Progress';
+// import Login from './assets/Login';
+// import Profile from './assets/Profile';
+// import Food from './assets/Food';
+// import Types from './assets/Types';
+// import Progress from './assets/Progress';
 import Navbar from './assets/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
+
         <Navbar />
         <Header />
 
-        </div>
+        <Route path = "/graph" render = {() => {
+          return(
+            <div>
+              <Graph />
+              <Body />
+            </div>
+          )
+        }}>
+        </Route>
+        
+
+        </Router>
     );
   };
 };
