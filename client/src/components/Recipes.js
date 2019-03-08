2d2cf095ef35e98f5aff3856930f8dcbaa7e692e
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-03-08 04:51:53 -0600

    more recipe work

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 // var input = 'pizza'
-var key = '30e77a591ab3a009323f28be315be367';
 
-
+const style = {
+    header:{
+        backgroundColor: "rgb(170, 37, 37)",
+        color: "white"
+    },
+    image:{
+        height: "200px",
+        width: "auto"
+    },
+    body:{
+        backgroundColor: "white"
+    }
+}
 class Recipes extends React.Component {
     state = {
         recipes: "",
@@ -11,44 +22,35 @@ class Recipes extends React.Component {
     }
 
     componentDidMount(){
+        this.getData();
+    }
+
+    getData(){
         let recipes = [];
         axios.get('https://www.food2fork.com/api/search?key=' + key + '&q=' + this.state.input)
             .then((response) => {
                 var divs = response.data.recipes.map((x) => {
                     console.log(x);
                     return(
-                    <div className = 'row'>
-                        <img className = 'col-4 img-fluid' src = {x.img_url} alt = {x.title} />
-                        <div className = 'col-8'>
-                            <a href = {x.source_url}>
-                                <h3>
-                                    {x.title}
-                                </h3>
-                            </a>
-                            <p>
-                                <em>Published By: {x.publisher}</em>
-                            </p>
+                        <div key = {x.recipe_id} className = 'row mb-4' style = {style.body}>
+                            <img className = 'col-4 img-fluid' src = {x.image_url} alt = {x.title} style = {style.image} />
+                            <div className = 'col-8'>
+                                <a href = {x.source_url}>
+                                    <h2 style = {style.header}>
+                                        {x.title}
+                                    </h2>
+                                </a>
+                                <p>
+                                    <em>Published By: <a href = {x.publisher_url}>{x.publisher}</a></em>
+                                    Recipe Id: {x.recipe_id}
+                                    Link: <a href = {x.source_url}>{x.source_url}</a>
+                                </p>
+                            </div>
                         </div>
-                    </div>
                     );
                 });
+
                 recipes.push(divs);
-
-                // for(var i = 0; i<response.data.count; i++){
-                //     var listNum = i + 1;
-                //     var firstLine = '\n*******RESULT #' + listNum + '*******\n';
-                //     var lastLine = '';
-
-                //     console.log(firstLine);
-                //     console.log('    Title: ' + response.data.recipes[i].title);
-                //     console.log('    ID#: ' + response.data.recipes[i].recipe_id);
-                //     console.log('    Link: ' + response.data.recipes[i].source_url + '\n');
-                    
-                //     for (var j = 0; j < firstLine.length; j++){
-                //         lastLine += '*';
-                //     };
-                //     console.log(lastLine);
-                // }
 
                 this.setState({
                     "recipes": recipes[0]
@@ -56,18 +58,31 @@ class Recipes extends React.Component {
             }).catch(function(error){throw error});
     }
     render(){
-        console.log(this.state.recipes)
-        if(!this.state.recipes === ""){
+        console.log(this.state)
+        if (this.state.recipes === "") {
             return(
                 <h3>Waiting for recipes...</h3>
             )
+        } else {
+            return(
+                <div>
+                    <div>
+                    <input 
+                        onChange = {
+                            (event) =>{this.setState({input: event.target.value})}
+                        }
+                    />
+                    <button
+                        onClick = {this.getData()}
+                    >Submit</button>
+                    </div>
+                    <div className = 'container'>
+                        <h1>Hello World!</h1>
+                        {this.state.recipes}
+                    </div>
+                </div>
+            )
         }
-        return(
-            <div className = 'container'>
-                <h1>Hello World!</h1>
-                {this.state.recipes}
-            </div>
-        )
     }
 }
 
