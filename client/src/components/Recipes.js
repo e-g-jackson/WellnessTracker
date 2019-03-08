c62b14babf21f39219bed63f661dcf1202f9c812
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-03-08 03:32:32 -0600

    recipes added

---
+++ client/src/components/Recipes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Recipes extends React.Component {
+    componentDidMount(){
+        
+    }
+    render(){
+        return(
+            <div className = 'container'>
+                <div className = 'row'>
+
+                </div>
+            </div>
+        )
+    }
+}
