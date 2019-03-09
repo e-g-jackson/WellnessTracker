04b214ea2e808ab6b8244d711a1e8aea751b0710
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-03-09 05:58:16 -0600

    got rid of extra backgrounds

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-var key = F2F_KEY;
 
-// let inputVal = "";
+var key = '30e77a591ab3a009323f28be315be367';
+// var key = F2F_KEY;
 
 const style = {
     header:{
